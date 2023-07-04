5be411f90d27a3d9d3f6c6791011f9b8256fa5b1
feiwhang <feiwhang@gmail.com>
2023-07-05 00:37:46 +0700

    refactor db model

---
+++ app/models/db.py
@@ -0,0 +1,9 @@
+from app import db
+
+
+class Student(db.Model):
+    __tablename__ = 'student'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
