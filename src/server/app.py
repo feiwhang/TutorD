eaefcc6bd843e379f9d1ae13d8eb0004d521e6eb
feiwhang <feiwhang@gmail.com>
2023-07-05 22:55:30 +0700

    serialize db models

--- src/server/app.py
+++ src/server/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
-from db import db
+from flask import Flask, jsonify
+from db import db, Course
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:iccsroot@db/tutord"
 
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/api/courses')
+def get_courses():
+    courses = Course.query.all()
+    return jsonify([course.serialize() for course in courses])
 
 
 if __name__ == '__main__':
--- src/server/db.py
+++ src/server/db.py
@@ -12,6 +12,14 @@ class Student(db.Model):
     email = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email
+        }
+
 
 class Tutor(db.Model):
     __tablename__ = 'tutor'
@@ -22,12 +30,26 @@ class Tutor(db.Model):
     email = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email
+        }
+
 
 class Course(db.Model):
     __tablename__ = 'course'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
 
 
 class Favourite(db.Model):
@@ -36,6 +58,13 @@ class Favourite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
     tutor_id = db.Column(db.Integer, db.ForeignKey('tutor.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'student_id': self.student_id,
+            'tutor_id': self.tutor_id
+        }
 
 
 class TutorCourse(db.Model):
@@ -47,6 +76,15 @@ class TutorCourse(db.Model):
     verification_status = db.Column(db.String(50), nullable=False)
     verified_by = db.Column(db.Integer, db.ForeignKey('admin.id'))
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'tutor_id': self.tutor_id,
+            'course_id': self.course_id,
+            'verification_status': self.verification_status,
+            'verified_by': self.verified_by
+        }
+
 
 class Admin(db.Model):
     __tablename__ = 'admin'
@@ -54,3 +92,9 @@ class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
