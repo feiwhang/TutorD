5a01124fe391b500eb762297175fcd3cf191d8d0
feiwhang <feiwhang@gmail.com>
2023-07-05 14:44:48 +0700

    crete bs base template

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from db import db, Student, Tutor
 
 app = Flask(__name__)
@@ -9,9 +9,7 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    students = Student.query.all()
-    tutors = Tutor.query.all()
-    return '<br>'.join([f'{student.id}: {student.first_name} {student.last_name} ({student.email})' for student in students] + [f'{tutor.id}: {tutor.first_name} {tutor.last_name} ({tutor.email})' for tutor in tutors])
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
