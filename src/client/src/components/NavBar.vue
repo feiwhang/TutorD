79cdda4f3f58fefa0d87cbbd6d8d8f1f56285786
feiwhang <feiwhang@gmail.com>
2023-07-14 01:02:17 +0700

    Navigate to login when log out

--- src/client/src/components/NavBar.vue
+++ src/client/src/components/NavBar.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { useUserStore } from "../store/user";
+import { useRouter } from "vue-router";
 
 const userStore = useUserStore();
+const router = useRouter();
 
 const logout = () => {
   userStore.logout();
+  router.push("/login");
 };
 </script>
 
